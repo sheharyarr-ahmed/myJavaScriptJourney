174c436b19f4cd3f1efcffef77760fc7b6998c0c
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-03-03 12:25:42 +0500

    first commit

---
+++ dataTypes.js
@@ -0,0 +1,25 @@
+// this section represents typeof function that represents the types of data types
+
+let sh = true;
+console.log(sh);
+
+let umer = "human";
+console.log(typeof umer);
+
+let money = 1000;
+console.log(typeof money);
+
+console.log(typeof sh);
+
+let uniName = "SZABIST";
+console.log(uniName);
+
+uniName = "NUST";
+console.log(uniName);
+console.log(typeof uniName);
+
+uniName = 56;
+console.log(typeof uniName);
+
+let unKnown;
+console.log(unKnown);
---
+++ variablesAndValues.js
@@ -0,0 +1,27 @@
+// this section represents the values and variables practices
+
+let country1 = "Pakistan";
+console.log(country1);
+
+let city = "Karachi";
+console.log(city);
+
+let countryContinent = "Asia";
+console.log(countryContinent);
+
+let pakistanPopulation = "241 million";
+console.log(pakistanPopulation);
+
+let pakistanContinent = "Asia";
+console.log(pakistanContinent);
+
+let pakistanCurrency = "Rupees";
+console.log(pakistanCurrency);
+
+let country = "Pakistan";
+let continent = "Asia";
+let population = "241 million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
