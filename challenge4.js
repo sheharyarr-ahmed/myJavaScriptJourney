20a165947af0beea3143183c525c752fca6c18db
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-03-06 02:38:36 +0500

    done with the challenge 4

---
+++ challenge4.js
@@ -0,0 +1,8 @@
+const bill = 275;
+const tip = bill >= 50 && bill <= 300 ? (bill * 15) / 100 : (bill * 20) / 100;
+
+console.log(
+  `the bill was ${bill} the tip was ${
+    bill >= 50 && bill <= 300 ? (bill * 15) / 100 : (bill * 20) / 100
+  } and the total value ${bill + tip}`
+);
