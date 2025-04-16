45c7a38352f99ff99f753546b596d97624e36533
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-04-16 12:06:17 +0500

    done work on for loops and practiced them, learned about how to change and replace values of arrays through loops, use of if else statement in the loops and the splice method

---
+++ loopUses.js
@@ -0,0 +1,75 @@
+"use strict";
+
+const egArray = new Array("shery", "grande", "million", [
+  "asif",
+  "umer",
+  "basit",
+]);
+
+// console.log(egArray.length - 1);
+// for (let i = 0; i < 4; i++) {
+//   console.log(egArray[i]);
+// }
+
+for (let i = 0; i < egArray.length; i++) {
+  console.log(egArray[i]);
+}
+
+const shery = new Array("shakeel ahmed", "bmw", "rolex", "civic");
+const newOne = ["shakeel ahmed", "ferrari", "pathek", "lc300"];
+console.log(shery.length - 1, newOne.length - 1);
+// for (let j = 0; )
+for (let j = 0; j < shery.length; j++) {
+  shery[j] = newOne[j];
+  //   console.log(shery);
+}
+console.log(shery);
+
+const anotherOne = ["lambo", "audi"];
+for (let q = 0; q < anotherOne.length; q++) {
+  shery.push(anotherOne[q]);
+}
+
+console.log(shery);
+
+// filling an empty array by replacing values from another arrray
+
+const emptyArray = [];
+for (let w = 0; w < shery.length; w++) {
+  emptyArray[w] = shery[w];
+}
+console.log(emptyArray);
+
+const money = [10, 100, 1000, 100000];
+const newMoney = [];
+
+for (let m = 0; m < money.length; m++) {
+  newMoney[m] = money[m] - 1;
+
+  if (m === 2) {
+    money[m] = "barca";
+  }
+}
+
+console.log(newMoney);
+console.log(money);
+
+for (let e = 0; e < money.length; e++) {
+  if (typeof money[e] === "string") break;
+  console.log(money[e]);
+}
+
+for (let l = 0; l < money.length; l++) {
+  if (typeof money[l] !== "number") continue;
+  console.log(money[l]);
+}
+
+const cars = ["bmw", "ferrari", "lc300"];
+cars.splice(1, 0, "civic");
+console.log(cars);
+
+for (let t = 0; t < cars.length; t++) {
+  if (t === 2) {
+    cars.splice(2, 0, "audi");
+  }
+}
