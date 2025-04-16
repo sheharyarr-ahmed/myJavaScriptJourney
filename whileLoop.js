37d209939f5d1987867f73ce22b5d3764d1393e2
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-04-16 13:16:50 +0500

    learned about while and implementation of while loops

---
+++ whileLoop.js
@@ -0,0 +1,16 @@
+"use strict";
+// let i = 1;
+// while (i <= 10) {
+//   console.log(`the number ${i}`);
+//   i++;
+// }
+
+let h = 1;
+while (h <= 10) {
+  if (h === 7) {
+    console.log(`the counter has reached ${h}`);
+  } else if (h !== 7) {
+    console.log(`the number is not 7 as it is ${h}`);
+  }
+  h++;
+}
