5fdc19d7e23ceb07578f740333a0edf61d1979ac
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-04-18 02:49:24 +0500

    upated

--- challenge.js
+++ challenge.js
@@ -1,13 +1,14 @@
 "use strict";
-// let fTemps1 = [17, 21, 23];
-// let fTemps2 = [12, 5, -5, 0, 4];
+let fTemps1 = [17, 21, 23];
+let fTemps2 = [12, 5, -5, 0, 4];
 
-// for (let t = 0; t < fTemps1.length; t++) {
-//   const printForecast = function (arr) {
-//     return `... ${fTemps1[t]} in ${[t + 1]} days`;
-//   };
-//   console.log(printForecast(fTemps1));
-// }
+for (let t = 0; t < fTemps1.length; t++) {
+  const printForecast = function (arr) {
+    return `... ${fTemps1[t]} in ${[t + 1]} days`;
+  };
+  console.log(printForecast(fTemps1));
+}
+
 const data1 = [17, 21, 23];
 const data2 = [12, 5, -5, 0, 4];
 
