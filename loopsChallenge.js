5defca5f5e24276369da228cd373ada7ee3de8bc
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-04-17 11:01:17 +0500

    donw with tthe challenge of while loops

--- loopsChallenge.js
+++ loopsChallenge.js
@@ -1,17 +1,32 @@
-"use strict";
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-let tips = [];
-let totals = [];
-const calcTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-};
+// "use strict";
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// let tips = [];
+// let totals = [];
+// const calcTip = function (bill) {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// };
 
-for (let i = 0; i < bills.length; i++) {
-  calcTip(bills[i]);
-  console.log(calcTip(bills[i]));
-  tips.push(calcTip(bills[i]));
+// for (let i = 0; i < bills.length; i++) {
+//   calcTip(bills[i]);
+//   console.log(calcTip(bills[i]));
+//   tips.push(calcTip(bills[i]));
 
-  totals.push(bills[i] + tips[i]);
+//   totals.push(bills[i] + tips[i]);
+// }
+// console.log(tips);
+// console.log(totals);
+
+let numbers = [100, 200, 300, 400];
+let average;
+function sum(nums) {
+  let total = 0;
+
+  for (let n = 0; n < numbers.length; n++) {
+    total += numbers[n];
+  }
+  return total;
 }
-console.log(tips);
-console.log(totals);
+const sumResult = sum(numbers);
+console.log(sumResult);
+average = sumResult / numbers.length;
+console.log(`the average is ${average}`);
